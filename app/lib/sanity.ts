b22bed81be4ab513e2d1a7ecf5a1411b6d7265c9
highfreshness <meteo0718@gmail.com>
2024-03-16 02:26:51 +0900

    first commit

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,12 +1,15 @@
 import { createClient } from "next-sanity";
 import ImageUrlBuilder from "@sanity/image-url";
 
+const projectid = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const token = process.env.SANITY_TOKEN;
+
 export const client = createClient({
     apiVersion: '2023-05-03',
     dataset: 'production',
-    projectId: 's3qx53uu',  // 유출위험
+    projectId: 'projectid',  // 유출위험
     useCdn: false,
-    token: 'skY8XrvMN1VZpdul59LtNDPYzlhHAPObIIvxt4Q3mQc99GZ0E7DnnORJFk5W1JOPUvcWGfyNu6UTSOZmAgnW6YIN5DHH01BUKieVNtmnyJCVBAZj7Jiajj9FXqNC7bPBQitfwx8z2z1NiloHISGvNEWEbjOoezkI1kcvMGbWrsjtHm3buRZK',
+    token: 'token',
 });
 
 const builder = ImageUrlBuilder(client);
